631885a89a58bc167390b7d18f11448cbc9f8fb5
Jason Scharff <jasonscharff@gmail.com>
2017-03-12 11:42:49 -0700

    Create empty user view controller

---
+++ Principium/MEHUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  MEHUserViewController.h
+//  Principium
+//
+//  Created by Jason Scharff on 3/12/17.
+//  Copyright © 2017 MenloHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MEHUserViewController : UIViewController
+
+@end
---
+++ Principium/MEHUserViewController.m
@@ -0,0 +1,37 @@
+//
+//  MEHUserViewController.m
+//  Principium
+//
+//  Created by Jason Scharff on 3/12/17.
+//  Copyright © 2017 MenloHacks. All rights reserved.
+//
+
+#import "MEHUserViewController.h"
+
+@interface MEHUserViewController ()
+
+@end
+
+@implementation MEHUserViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
